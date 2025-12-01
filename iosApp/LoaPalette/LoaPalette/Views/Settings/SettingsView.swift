0c92b28ac3e355f489c8888a4d9b114e401ecb99
tsy <t2.locoo.k4@gmail.com>
2025-12-01 22:41:54 +0900

    設定画面

--- iosApp/LoaPalette/LoaPalette/Views/Settings/SettingsView.swift
+++ iosApp/LoaPalette/LoaPalette/Views/Settings/SettingsView.swift
@@ -7,18 +7,78 @@
 
 import SwiftUI
 
+private enum SettingsItem: Identifiable {
+    case officialSite
+    case contact
+
+    var id: String {
+        switch self {
+        case .officialSite:
+            return "officialSite"
+        case .contact:
+            return "contact"
+        }
+    }
+}
+
 struct SettingsView: View {
+    private let items: [SettingsItem] = [
+        .officialSite,
+        .contact,
+    ]
+
     var body: some View {
         NavigationStack {
             Form {
                 Section {
-                    ForEach(0..<10) { item in
-                        Text("item\(item)")
+                    ForEach(items) { item in
+                        switch item {
+                        case .officialSite:
+                            officialSite()
+                        case .contact:
+                            contact()
+                        }
                     }
                 }
             }
             .scrollContentBackground(.hidden)
             .navigationTitle(String(localized: "設定"))
+        }
+    }
+
+    private func officialSite() -> some View {
+        Link(
+            destination: URL(
+                string: "https://www.takaratomy.co.jp/products/disneylorcana/")!
+        ) {
+            cell(
+                title: String(localized: "公式サイト"),
+                systemName: "chevron.right"
+            )
+        }
+    }
+
+    private func contact() -> some View {
+        // TODO: GoogleForm作成
+        Link(
+            destination: URL(string: "https://example.com/contact")!
+        ) {
+            cell(
+                title: String(localized: "要望/お問い合わせ"),
+                systemName: "chevron.right"
+            )
+        }
+    }
+
+    private func cell(title: String, systemName: String?) -> some View {
+        HStack {
+            Text(title)
+                .foregroundColor(.white)
+            Spacer()
+            if let systemName {
+                Image(systemName: systemName)
+                    .foregroundColor(.white)
+            }
         }
     }
 }
